9e564696b24abb0896f56401b28f5cbcae4b92d6
KarishmaGhiya <kaghiya@microsoft.com>
2025-09-17 16:23:31 -0700

    Add readme for npm package (#451)

--- eng/npm/wrapper/scripts/post-install-script.js
+++ eng/npm/wrapper/scripts/post-install-script.js
@@ -8,6 +8,6 @@ const requiredPackage = `@azure/mcp-${platform}-${arch}`;
 try {
   require.resolve(requiredPackage);
 } catch (err) {
-  console.error(`Missing required package: '${requiredPackage}'`);
+  console.error(`Missing required package: '${requiredPackage}'. Follow the troubleshooting steps - https://aka.ms/azmcp/troubleshooting#platform-package-installation-issues`);
   process.exit(1);
 }
